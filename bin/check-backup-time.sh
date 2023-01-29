c41fe861ceabc60d0ca9c2f04ff8d691cea769ff
mathieu-besson <mathieubesson2001@gmail.com>
2023-01-30 00:12:04 +0100

    :recycle: Refactor : Création d'une fonction principale dans le script check_backup_time

--- bin/check-backup-time.sh
+++ bin/check-backup-time.sh
@@ -4,16 +4,20 @@
 
 source $(dirname $(realpath ${BASH_SOURCE[0]}))/../utils/pre-script.sh
 
-for KEY in "${!SERVERS[@]}"; do
-    eval "${SERVERS["$KEY"]}"
-    if [[ ${SERVERS[NAME]} == $HOST ]]; then 
-        break
+check_backup_time(){
+    for KEY in "${!SERVERS[@]}"; do
+        eval "${SERVERS["$KEY"]}"
+        if [[ ${SERVERS[NAME]} == $HOST ]]; then 
+            break
+        fi
+    done
+
+    timestamp_last_backup=$(cat ${SERVER[FOLDER_BACKUP_PARAMETERS]}/timestamp-last-backup)
+    timestamp_backup_warning=$(date -d "-${SERVER[NUMBER_OF_DAYS_WITHOUT_WARNING]} days" +"%s")
+
+    if [[ $timestamp_last_backup < $timestamp_backup_warning ]]; then
+        discord_notify "danger" "🚨 Backup" "▶️  Pas de backup de ${SERVER[NAME]} (${SERVER[IP]}) depuis ${SERVER[NUMBER_OF_DAYS_WITHOUT_WARNING]} jours, bouge toi !"
     fi
-done
+}
 
-timestamp_last_backup=$(cat ${SERVER[FOLDER_BACKUP_PARAMETERS]}/timestamp-last-backup)
-timestamp_backup_warning=$(date -d "-${SERVER[NUMBER_OF_DAYS_WITHOUT_WARNING]} days" +"%s")
-
-if [[ $timestamp_last_backup < $timestamp_backup_warning ]]; then
-    discord_notify "danger" "🚨 Backup" "▶️  Pas de backup de ${SERVER[NAME]} (${SERVER[IP]}) depuis ${SERVER[NUMBER_OF_DAYS_WITHOUT_WARNING]} jours, bouge toi !"
-fi
+check_backup_time
